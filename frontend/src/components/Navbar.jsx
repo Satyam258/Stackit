a40eb0905ab4fe2a0c09e52feb1b93044709898b
Satyam Singh <satyamsinghksh@gmail.com>
2025-07-12 12:37:02 +0530

    feat: add Poppins font and update Navbar styles

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Bell, UserCircle } from 'lucide-react'
 import { Link } from 'react-router-dom'
+import "@fontsource/poppins"
 
 function Navbar() {
   const isLoggedIn = false 
@@ -8,13 +9,13 @@ function Navbar() {
   return (
     <nav className="flex justify-between items-center px-6 py-4 shadow-md bg-gray-400 sticky top-0 z-50">
       {/* Logo */}
-      <Link to="/" className="text-2xl  text-black text-shadow-amber-950 font-extrabold font-stretch-50%">
+      <Link to="/" className="text-2xl  text-black text-shadow-amber-950 font-extrabold font-stretch-50%" style={{fontFamily:"poppins"}}>
         StackIt
       </Link>
 
       {/* Center Nav Links  */}
       <div className="hidden md:flex gap-6 text-gray-800 font-medium">
-        <Link to="/ask" className="hover:text-blue-800 transition">Ask Question</Link>
+        <Link to="/ask" className="hover:text-blue-800 transition text-xl" style={{fontFamily:"roboto"}}>Ask Question</Link>
       </div>
 
       {/* Right Side */}
